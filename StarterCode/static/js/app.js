1203687223f89c3944e16494eece6dbb26fafc8e
safieoscar <65041538+safieoscar@users.noreply.github.com>
2020-08-27 23:34:58 -0400

    first draft

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -2,3 +2,22 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+var tbody = d3.select('tbody');
+
+//Loop through data in order to append table to html file
+function displayData(data) {
+    tbody.text("")
+    data.forEach(function(ufo_sighting) {
+    new_tr = tbody.append('tr')
+    Object.entries(ufo_sighting).forEach(function([key,value]){
+        new_td = new_tr.append('td').text(value)
+    }
+    )    
+    })
+}
+displayData(tableData);
+
+var filter = d3.select('#filter-btn');
+var 
+
+
